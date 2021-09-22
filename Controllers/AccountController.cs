3338db6b48eba06598781b36162e4545a1a6ff09
David An <David.An@mbp-2020.lan>
2021-09-21 22:27:14 -0400

    first commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWSLambdaCognitoOpenIDConnect.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult LogOut()
+        {
+            return SignOut(new string[] { 
+                CookieAuthenticationDefaults.AuthenticationScheme, 
+                OpenIdConnectDefaults.AuthenticationScheme 
+            });
+        }
+
+        public IActionResult LoggedOut()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWSLambdaCognitoOpenIDConnect.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
