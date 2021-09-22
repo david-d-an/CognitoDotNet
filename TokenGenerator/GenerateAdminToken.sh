46cac17dcf0cda68c9597404768c5dd0ac01cb24
David An <david.an@virbela.com>
2021-09-22 18:08:05 -0400

    Add script for token gerneation

---
+++ TokenGenerator/GenerateAdminToken.sh
@@ -0,0 +1,2 @@
+aws cognito-idp admin-initiate-auth --region us-east-2 --cli-input-json file://auth_admin.json > ./Log/AuthAdminResult.txt
+
---
+++ TokenGenerator/GenerateUserToken.sh
@@ -0,0 +1,1 @@
+aws cognito-idp initiate-auth --region us-east-2 --cli-input-json file://auth_user.json > ./Log/AuthUserResult.txt
