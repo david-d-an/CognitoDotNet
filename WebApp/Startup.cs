ed43d82857fdab49cc318aa5b58af08704e523e2
David An <David.An@mbp-2020.lan>
2021-09-21 22:59:51 -0400

    Reorganize folder structure

---
+++ WebApp/Startup.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebAppCognito
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; private set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var clientId = Configuration["Authentication:Cognito:ClientId"];
+            var clientSecret = Configuration["Authentication:Cognito:ClientSecret"];
+            var baseUrl = Configuration["Authentication:Cognito:BaseUrl"];
+            var logOutUrl = Configuration["Authentication:Cognito:LogOutUrl"];
+            var saveToken = bool.Parse(Configuration["Authentication:Cognito:SaveToken"]);
+            var responseType = Configuration["Authentication:Cognito:ResponseType"];
+            var metadataAddress = Configuration["Authentication:Cognito:MetadataAddress"];
+
+            services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+            .AddCookie(options => {
+                options.Events.OnSigningIn = FilterGroupClaims;
+                options.SlidingExpiration = true;
+            })
+            .AddOpenIdConnect(options => {
+                options.ResponseType = responseType;
+                options.MetadataAddress = metadataAddress;
+                options.ClientId = clientId;
+                options.ClientSecret = clientSecret;
+                options.SaveTokens = saveToken;
+                options.GetClaimsFromUserInfoEndpoint = true;
+                // options.SignedOutRedirectUri = "https://localhost:15156/Account/LoggedOut/";
+                options.Scope.Add("email");
+                options.Events = new OpenIdConnectEvents {
+                    OnRedirectToIdentityProviderForSignOut = (context) => {
+                        var logoutUri = logOutUrl;
+                        logoutUri += $"?client_id={clientId}&logout_uri={baseUrl}/Account/LoggedOut/";
+                        context.Response.Redirect(logoutUri);
+                        context.HandleResponse();
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+
+            // var clientSecret = Configuration["AmazonCognito:ClientSecret"];
+            // var clientId = Configuration["AmazonCognito:ClientId"];
+            // var metadataAddress = Configuration["AmazonCognito:MetaDataAddress"];
+            // var logOutUrl = Configuration["AmazonCognito:LogOutUrl"];
+            // var baseUrl = Configuration["AmazonCognito:BaseUrl"];
+            
+            // services.AddAuthentication(options =>
+            // {
+            //     options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            //     options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            //     options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            // })
+            // .AddCookie(options => {
+            //     options.Events.OnSigningIn = FilterGroupClaims;
+            //     options.SlidingExpiration = true;
+            // })
+            // .AddOpenIdConnect(options =>
+            // {
+            //     options.ResponseType = "code";
+            //     options.MetadataAddress = metadataAddress;
+            //     options.ClientId = clientId;
+            //     // options.SaveTokens = true;
+            //     options.ClientSecret = clientSecret;
+            //     options.GetClaimsFromUserInfoEndpoint = true;
+            //     options.Scope.Add("email");
+            //     options.Events = new OpenIdConnectEvents
+            //     {
+            //         OnRedirectToIdentityProviderForSignOut = (context) =>
+            //         {
+            //             var logoutUri = logOutUrl;
+            //             logoutUri += $"?client_id={clientId}&logout_uri={baseUrl}";
+            //             context.Response.Redirect(logoutUri);
+            //             context.HandleResponse();
+            //             return Task.CompletedTask;
+            //         }
+            //     };
+            // });
+
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // if (env.IsDevelopment()) {
+            //     app.UseDeveloperExceptionPage();
+            // }
+            // else {
+            //     app.UseExceptionHandler("/Error");
+            //     app.UseHsts();
+            // }
+
+            // app.UseAuthentication();
+            // // app.UseAuthorization();
+
+            // app.UseHttpsRedirection();
+            // app.UseStaticFiles();
+            // app.UseCookiePolicy();
+
+            // app.UseMvc(routes => {
+            //     routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            // });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseAuthentication();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes => {
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        //Remove all the claims that are unrelated to our identity
+        private static Task FilterGroupClaims(CookieSigningInContext context) {
+            var principal = context.Principal;
+            if (principal.Identity is ClaimsIdentity identity)
+            {
+                var unused = identity.FindAll(GroupsToRemove).ToList();
+                unused.ForEach(c => identity.TryRemoveClaim(c));
+            }
+            return Task.FromResult(principal);
+        }
+
+        private static bool GroupsToRemove(Claim claim) {
+            string[] _groupObjectIds = new string[] { "identities" };
+            return claim.Type == "groups" && !_groupObjectIds.Contains(claim.Type);
+        }
+
+    }
+}
