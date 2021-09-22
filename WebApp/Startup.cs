80757764e38a229279a1fb86bb08bbee12f75640
David An <david.an@virbela.com>
2021-09-22 10:49:59 -0400

    Clean StartUp.cs. Replace UseMvc with UseEndPoint routing.

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -64,95 +64,42 @@ namespace WebAppCognito
                 };
             });
 
-            // var clientSecret = Configuration["AmazonCognito:ClientSecret"];
-            // var clientId = Configuration["AmazonCognito:ClientId"];
-            // var metadataAddress = Configuration["AmazonCognito:MetaDataAddress"];
-            // var logOutUrl = Configuration["AmazonCognito:LogOutUrl"];
-            // var baseUrl = Configuration["AmazonCognito:BaseUrl"];
-            
-            // services.AddAuthentication(options =>
-            // {
-            //     options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-            //     options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-            //     options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-            // })
-            // .AddCookie(options => {
-            //     options.Events.OnSigningIn = FilterGroupClaims;
-            //     options.SlidingExpiration = true;
-            // })
-            // .AddOpenIdConnect(options =>
-            // {
-            //     options.ResponseType = "code";
-            //     options.MetadataAddress = metadataAddress;
-            //     options.ClientId = clientId;
-            //     // options.SaveTokens = true;
-            //     options.ClientSecret = clientSecret;
-            //     options.GetClaimsFromUserInfoEndpoint = true;
-            //     options.Scope.Add("email");
-            //     options.Events = new OpenIdConnectEvents
-            //     {
-            //         OnRedirectToIdentityProviderForSignOut = (context) =>
-            //         {
-            //             var logoutUri = logOutUrl;
-            //             logoutUri += $"?client_id={clientId}&logout_uri={baseUrl}";
-            //             context.Response.Redirect(logoutUri);
-            //             context.HandleResponse();
-            //             return Task.CompletedTask;
-            //         }
-            //     };
-            // });
-
-            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddControllersWithViews();
+            services.AddAuthorization();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            // if (env.IsDevelopment()) {
-            //     app.UseDeveloperExceptionPage();
-            // }
-            // else {
-            //     app.UseExceptionHandler("/Error");
-            //     app.UseHsts();
-            // }
-
-            // app.UseAuthentication();
-            // // app.UseAuthorization();
-
-            // app.UseHttpsRedirection();
-            // app.UseStaticFiles();
-            // app.UseCookiePolicy();
-
-            // app.UseMvc(routes => {
-            //     routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
-            // });
-
-            if (env.IsDevelopment())
-            {
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
+            else {
                 app.UseExceptionHandler("/Error");
                 app.UseHsts();
             }
 
-            app.UseAuthentication();
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
 
-            app.UseMvc(routes => {
-                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            app.UseRouting();
+
+            app.UseCors();
+
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { 
+                endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}"); 
             });
         }
 
         //Remove all the claims that are unrelated to our identity
         private static Task FilterGroupClaims(CookieSigningInContext context) {
             var principal = context.Principal;
-            if (principal.Identity is ClaimsIdentity identity)
-            {
+            if (principal.Identity is ClaimsIdentity identity) {
                 var unused = identity.FindAll(GroupsToRemove).ToList();
                 unused.ForEach(c => identity.TryRemoveClaim(c));
             }
@@ -163,6 +110,5 @@ namespace WebAppCognito
             string[] _groupObjectIds = new string[] { "identities" };
             return claim.Type == "groups" && !_groupObjectIds.Contains(claim.Type);
         }
-
     }
 }
