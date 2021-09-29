249edf1433e426323d2106cdb57ce2b44498a2dc
David An <david.an@virbela.com>
2021-09-29 13:08:21 -0400

    Update scripts to work with app client with/without client secret

--- TokenGenerator/GenerateAdminToken.sh
+++ TokenGenerator/GenerateAdminToken.sh
@@ -1,2 +1,7 @@
-aws cognito-idp admin-initiate-auth --region us-east-2 --cli-input-json file://auth_admin.json > ./Log/AuthAdminResult.txt
+echo "Auth result by Cognito with Client Secret" > ./Log/AuthAdminResult.txt 
+aws cognito-idp admin-initiate-auth --region us-east-2 --cli-input-json file://Credentials/auth_admin_ClientSecret.json >> ./Log/AuthAdminResult.txt
 
+echo >> ./Log/AuthAdminResult.txt
+
+echo "Auth result by Cognito without Client Secret" >> ./Log/AuthAdminResult.txt 
+aws cognito-idp admin-initiate-auth --region us-east-2 --cli-input-json file://Credentials/auth_admin_No_ClientSecret.json >> ./Log/AuthAdminResult.txt
--- TokenGenerator/GenerateUserToken.sh
+++ TokenGenerator/GenerateUserToken.sh
@@ -1,1 +1,7 @@
-aws cognito-idp initiate-auth --region us-east-2 --cli-input-json file://auth_user.json > ./Log/AuthUserResult.txt
+echo "Auth result by Cognito with Client Secret" > ./Log/AuthUserResult.txt
+aws cognito-idp initiate-auth --region us-east-2 --cli-input-json file://Credentials/auth_user_ClientSecret.json >> ./Log/AuthUserResult.txt
+
+echo >> ./Log/AuthUserResult.txt
+
+echo "Auth result by Cognito without Client Secret" >> ./Log/AuthUserResult.txt
+aws cognito-idp initiate-auth --region us-east-2 --cli-input-json file://Credentials/auth_user_No_ClientSecret.json >> ./Log/AuthUserResult.txt
